61ad25059dbe10ecbc3a42f6446ea72d74c07c80
DJ-Yang <dong3831@hanmail.net>
2020-05-22 02:05:11 +0900

    server side data loading with redux-thunk and preloadcontext

--- src/index.server.js
+++ src/index.server.js
@@ -1,22 +1,27 @@
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-import express from "express";
-import { StaticRouter } from "react-router-dom";
-import App from "./App";
-import path from "path";
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
+import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
 
 // asset-mainfest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
-  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
-)
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8'),
+);
 
 const chunks = Object.keys(manifest.files)
-  .filter(key => /chunk\.js$/.exec(key))  // chunk.js로 끝나는 키를 찾아서
-  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
-  .join('');  // 합침
+  .filter((key) => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(''); // 합침
 
-function createPage(root) {
+function createPage(root, stateScript) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -35,6 +40,7 @@ function createPage(root) {
     <div id="root">
       ${root}
     </div>
+    ${stateScript}
     <script src="${manifest.files['runtime~main.js']}"></script>
     ${chunks}
     <script src="${manifest.files['main.js']}"></script>
@@ -46,17 +52,38 @@ function createPage(root) {
 const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
-const serverRender = (req, res, next) => {
+const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
   const context = {};
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+
+  const preloadContext = {
+    done: false,
+    promises: [],
+  };
   const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
   );
+
+  ReactDOMServer.renderToStaticMarkup(jsx); // 처음 한번 렌더링 합니다.
+  try {
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
   const root = ReactDOMServer.renderToString(jsx); // 렌더링 진행
-  res.send(createPage(root)); // 클라이언트에게 결과물을 응답합니다.
+  // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+  const stateString = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
+  const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`;  // 리덕스 초기 상태를 스크립트로 주입합니다.
+  res.send(createPage(root, stateScript)); // 클라이언트에게 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
@@ -68,5 +95,5 @@ app.use(serverRender);
 
 // 5000 포트로 서버를 가동합니다.
 app.listen(5000, () => {
-  console.log("Running on http://localhost:5000");
+  console.log('Running on http://localhost:5000');
 });
